9a0fabe6df49d093f2cc699b1a76d18b0b563df2
Evann Li <evannli450@gmail.com>
2024-04-08 10:03:13 -0700

    labexample

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,9 +3,9 @@ const MongoClient = require("mongoose")
 const is_render = process.env.IS_RENDER || false;
 
 
-const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.hwmliaf.mongodb.net/ "+lab_example+"?retryWrites=true&w=majority"
+const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.hwmliaf.mongodb.net/lab_example?retryWrites=true&w=majority"
 
-const localURI = "mongodb://localhost/"+lab_example+"?authSource=admin&retryWrites=true" 
+const localURI = "mongodb://localhost/lab_example?authSource=admin&retryWrites=true" 
 
 if (is_render) {
 	var database = new MongoClient(renderURI, {useNewUrlParser: true, useUnifiedTopology: true});
